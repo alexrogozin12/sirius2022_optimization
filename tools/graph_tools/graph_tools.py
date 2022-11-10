52e63e63aefbe4dc7a84fcb5f2686deb8d70d9a8
YADRO\a.saratovtsev <a.saratovtsev@yadro.com>
2022-11-10 12:09:08 +0300

    fix metropolis weights

--- tools/graph_tools/graph_tools.py
+++ tools/graph_tools/graph_tools.py
@@ -5,6 +5,39 @@ import networkx as nx
 from networkx.generators.random_graphs import erdos_renyi_graph, complete_graph
 from networkx.generators import circulant_graph
 
+def vanish_random_edges_from_graph(G, occupancy=0.0, vanish_rate = 0):
+    if occupancy*vanish_rate != 0: raise ValueError()
+    edges = list(G.edges)
+    num_of_edges = len(edges)
+    if occupancy != 0: vanish_rate = int(np.round(num_of_edges*occupancy))
+    if num_of_edges - vanish_rate < G.number_of_nodes() - 1:
+        print("vanish_rate more than N", vanish_rate, " ", G.number_of_nodes() - 1)
+        return
+    for i in range(vanish_rate):
+        # TODO: make this more optimal
+        # MB - DONE but not sure (self-loop nodes could be removed earlier, but I this that is it slower way)
+        while True:
+            edge = edges[random.randint(0, num_of_edges-1)]
+            G.remove_edge(*edge)
+            edges.remove(edge)
+            num_of_edges -= 1
+            if nx.is_connected(G) and edge[0] != edge[1]:
+                break
+            else:
+                G.add_edge(edge[0], edge[1], weight=1)
+
+    return nx.adjacency_matrix(G).todense()
+
+def vanish_random_edges_from_matrix(M, occupancy=0.0, vanish_rate = 0):
+    G = nx.from_numpy_matrix(M)
+    M = vanish_random_edges_from_graph(G, occupancy, vanish_rate)
+    return fill_metropolis_weigts(M)
+
+def vanish_random_edge_from_matrix(M):
+    G = nx.from_numpy_matrix(M)
+    M = vanish_random_edges_from_graph(G, occupancy=0.0, vanish_rate=1)
+    return fill_metropolis_weigts(M)
+
 def make_cirvular_graph(N):
     G = nx.circulant_graph(N, [1])
     return nx.adjacency_matrix(G).todense()
@@ -13,48 +46,10 @@ def make_complete_graph(N):
     G = nx.complete_graph(N)
     return nx.adjacency_matrix(G).todense()
 
-def make_random_graph(N, occupancy=0.5):
+def make_random_graph(N, occupancy=0.0, vanish_rate = 0):
     G = nx.complete_graph(N)
-    fixed_edges = []
-    edges = list(G.edges)
-    num_of_edges = len(edges)
-    vanish_rate = int(np.round(num_of_edges*occupancy))
-    if vanish_rate < N:
-        print("vanish_rate lover than N", vanish_rate, " ", N)
-        return
-    for i in range(vanish_rate):
-        # TODO: make this more optimal
-        while True:
-            edge = edges[random.randint(0, num_of_edges-1)]
-            if edge not in fixed_edges:
-                G.remove_edge(edge[0], edge[1])
-            if nx.is_connected(G):
-                edges.remove(edge)
-                num_of_edges -= 1
-                break
-            else:
-                G.add_edge(edge, weight=1)
-                fixed_edges.append(edge)
-    return nx.adjacency_matrix(G).todense()
-
-def vanish_random_edge(M):
-    G = nx.from_numpy_matrix(M)
-    fixed_edges = []
-    edges = list(G.edges)
-    num_of_edges = len(edges)
-    # TODO: make this more optimal
-    while True:
-        edge = edges[random.randint(0, num_of_edges-1)]
-        if edge not in fixed_edges:
-            G.remove_edge(edge[0], edge[1])
-        if nx.is_connected(G):
-            edges.remove(edge)
-            num_of_edges -= 1
-            break
-        else:
-            G.add_edge(edge[0], edge[1], weight=1)
-            fixed_edges.append(edge)
-    return fill_metropolis_weigts(nx.adjacency_matrix(G).todense())
+    M = vanish_random_edges_from_graph(G, occupancy, vanish_rate)
+    return fill_metropolis_weigts(M)
 
 def fill_metropolis_weigts(M):
     n_links = []
@@ -62,19 +57,22 @@ def fill_metropolis_weigts(M):
         n_links.append(np.count_nonzero(M[i]))
     G = nx.from_numpy_matrix(M)
     for (u, v) in G.edges():
-        G.edges[u,v]['weight'] = 1 / max(n_links[u], n_links[v])
+        G.edges[u,v]['weight'] = 1 / (1 + max(n_links[u], n_links[v]))
+    M = nx.adjacency_matrix(G).todense()
+    for i in range(len(M)):
+        G.add_edge(i, i, weight=(1 - np.sum(M[i])))
     return nx.adjacency_matrix(G).todense()
 
 def make_graph_img(M, num_of_lines = 5, fig_size=(5, 5)):
     G = nx.from_numpy_matrix(M)
     _w = []
     _step = 1 / num_of_lines
-    w_08 = [(u, v) for (u, v, d) in G.edges(data=True) if d["weight"] >= 0.8]
+
     for i in range(num_of_lines):
         _w.append([(u, v) for (u, v, d) in G.edges(data=True) if d["weight"] > i * _step and d["weight"] <= (i+1)*_step])
-    
-    pos = nx.spring_layout(G, seed=7)  # positions for all nodes - seed for reproducibility
 
+#    pos = nx.spring_layout(G, seed=7)  # positions for all nodes - seed for reproducibility
+    pos = nx.spring_layout(G)
     edge_labels = nx.get_edge_attributes(G, "weight")
     for key in edge_labels:
         edge_labels[key] = np.round(edge_labels[key], 2)
@@ -88,7 +86,7 @@ def make_graph_img(M, num_of_lines = 5, fig_size=(5, 5)):
 
     nx.draw_networkx_labels(G, pos, font_size=20, font_family="sans-serif")
     nx.draw_networkx_edge_labels(G, pos, edge_labels)
-
+    
     ax = plt.gca()
     ax.margins(0.08)
     plt.axis("off")
